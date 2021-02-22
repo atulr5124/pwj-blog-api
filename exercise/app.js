55a3cd3879250d40dbbac0165ca21d84c323c6a2
Atul Rauthan <atulrauthan7@gmail.com>
2021-02-22 16:16:13 +0530

    upload file functionality

--- exercise/app.js
+++ exercise/app.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const app = express()
 var cors = require('cors')
+var multer = require('multer')
 const Post = require('./api/models/posts')
 const postsData = new Post()
+var storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null, './uploads')
+    },
+    filename: function(req,file,cb) {
+        cb(null, file.fieldname + `${file.fieldname}-${Date.now()}${getExtension(file.mimetype)}`)
+    }
+})
+
+const getExtension = (mimeType) => {
+    switch(mimeType) {
+        case "image/png":
+            return ".png"
+        case "image/jpeg":
+            return ".jpeg"
+    }
+}
+
+var upload = multer({storage: storage})
 
 app.use(cors())
+app.use(express.json())
 
 app.use('/uploads', express.static('uploads'))
 
@@ -20,6 +41,18 @@ app.get("/api/posts/:post_id", (req, res) => {
     } else {
         res.status(404).send("Post not found.")
     }
+})
+
+app.post("/api/posts", upload.single("post_image"), (req, res) => {
+    const newPost = {
+        "id": Date.now(),
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path,
+        "added_date": Date.now()
+    }
+    postsData.add(newPost)
+    res.status(201).send(newPost)
 })
 
 app.listen(3000, () => {
